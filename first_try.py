9c96d0bf8b27b53082292136b5ffa8f0527ca86e
ArtemB118 <1evkakii1@gmail.com>
2023-06-11 19:49:26 +0600

    added second task

--- first_try.py
+++ first_try.py
@@ -14,3 +14,30 @@ intelligence_dict = {
     if hero["name"] in compare_names
 }
 print(max(intelligence_dict))
+
+with open ('Token.txt', 'r') as file:
+    tok = file.readline()
+
+class YaUploader:
+    def __init__(self, token: str):
+        self.token = token
+
+    def upload(self, file_path: str):
+        url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        param = {
+            'path': file_path
+        }
+        headers = {
+            'Authorization': token
+        }
+        respounce = requests.get(url, params=param, headers=headers)
+        url_upload = respounce.json().get('href', '')
+        with open (file_path, "br") as the_best:
+            upload_respounce = requests.put(url_upload, files={'file': the_best})
+
+if __name__ == '__main__':
+    path_to_file = 'gosling.jpg'
+    token = tok
+    uploader = YaUploader(token)
+    result = uploader.upload(path_to_file)
+    # в пути файла содержется название файла ?
